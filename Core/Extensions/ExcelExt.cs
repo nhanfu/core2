dc22e463980e230fc0eae662be1af0bb20c27fa0
PmkSoaiMuoi <57052389+PmkSoaiMuoi@users.noreply.github.com>
2023-02-13 16:52:20 +0700

    fix ExcelExt to xls extension

--- Core/Extensions/ExcelExt.cs
+++ Core/Extensions/ExcelExt.cs
@@ -23,7 +23,7 @@ namespace Core.Extensions
             }
             /*@
                 var dataType = "application/vnd.ms-excel";
-                var extension = ".xlsx";
+                var extension = ".xls";
                 let base64 = function(s) {
                     return window.btoa(unescape(encodeURIComponent(s)))
                 };
