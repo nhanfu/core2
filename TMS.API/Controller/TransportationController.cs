4df2f77b65ab68ad8be0a113682abda892b0abb9
Tinnt <nguyentrantindev@gmail.com>
2023-04-10 10:11:10 +0700

    fix export excel

--- TMS.API/Controller/TransportationController.cs
+++ TMS.API/Controller/TransportationController.cs
@@ -519,7 +519,7 @@ namespace TMS.API.Controllers
                     from Transportation t
                     left join Vendor b on b.Id = t.BossId
                     left join Location r on r.Id = {(transportation.TypeId == 2 ? "t.ReturnId" : "t.ReceivedId")}
-                    left join Location pi on pi.Id = {(transportation.TypeId == 2 ? "t.PickupEmptyId" : "t.ReturnEmptyId")}
+                    left join Location pi on pi.Id = {(transportation.TypeId == 2 ? "t.ReturnEmptyId" : "t.PickupEmptyId")}
                     left join Location po on po.Id = {(transportation.TypeId == 2 ? "t.PortLiftId" : "t.PortLoadingId")}";
             }
             else
@@ -561,7 +561,8 @@ namespace TMS.API.Controllers
                 }
                 else
                 {
-                    sql += $" where t.ClosingDate >= '{transportation.FromDate.Value.ToString("yyyy-MM-dd")}' and t.ClosingDate <= '{transportation.ToDate.Value.ToString("yyyy-MM-dd")}' and t.ClosingId = {transportation.ClosingId} and t.RouteId in ({transportation.RouteIds.Combine()})";
+                    sql += $" where t.ClosingDate >= '{transportation.FromDate.Value.ToString("yyyy-MM-dd")}' and t.ClosingDate <= '{transportation.ToDate.Value.ToString("yyyy-MM-dd")}' and t.ClosingId = {transportation.ClosingId} and t.RouteId in ({transportation.RouteIds.Combine()})"
+                    + $"  order by t.OrderExcel asc";
                 }
             }
             else
@@ -573,7 +574,8 @@ namespace TMS.API.Controllers
                 }
                 else
                 {
-                    sql += $" where t.ReturnDate >= '{transportation.FromDate.Value.ToString("yyyy-MM-dd")}' and t.ReturnDate <= '{transportation.ToDate.Value.ToString("yyyy-MM-dd")}' and t.ReturnVendorId = {transportation.ClosingId} and t.RouteId in ({transportation.RouteIds.Combine()})";
+                    sql += $" where t.ReturnDate >= '{transportation.FromDate.Value.ToString("yyyy-MM-dd")}' and t.ReturnDate <= '{transportation.ToDate.Value.ToString("yyyy-MM-dd")}' and t.ReturnVendorId = {transportation.ClosingId} and t.RouteId in ({transportation.RouteIds.Combine()})"
+                    + $"  order by t.OrderExcel asc";
                 }
             }
             var data = await ConverSqlToDataSet(sql);
