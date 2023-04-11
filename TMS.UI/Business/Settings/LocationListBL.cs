f7264d61b5ed6671a2556566101c6a854cffb373
Tinnt <nguyentrantindev@gmail.com>
2023-04-11 10:24:12 +0700

    add CustomMenu

--- TMS.UI/Business/Settings/LocationListBL.cs
+++ TMS.UI/Business/Settings/LocationListBL.cs
@@ -1,5 +1,6 @@
-﻿using Bridge.Html5;
+﻿using Bridge;
 using Core.Clients;
+using Core.Components;
 using Core.Components.Extensions;
 using Core.Components.Forms;
 using Core.Enums;
@@ -7,6 +8,7 @@ using Core.Extensions;
 using Core.MVVM;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TMS.API.Models;
 
@@ -14,30 +16,9 @@ namespace TMS.UI.Business.Settings
 {
     public class LocationListBL : TabEditor
     {
-        private HTMLInputElement _uploader;
-        private HTMLInputElement _uploaderPortLowerliftGoods;
-        private HTMLInputElement _uploaderPortLowerliftHollow;
         public LocationListBL() : base(nameof(TMS.API.Models.Location))
         {
             Name = "Location List";
-            DOMContentLoaded += () =>
-            {
-                Html.Take("Body").Form.Attr("method", "POST").Attr("enctype", "multipart/form-data")
-                .Display(false).Input.Event(EventType.Change, async (ev) => await SelectedExcel(ev)).Type("file").Id($"id_{GetHashCode()}").Attr("name", "fileImport").Attr("accept", ".xlsx");
-                _uploader = Html.Context as HTMLInputElement;
-            };
-            DOMContentLoaded += () =>
-            {
-                Html.Take("Body").Form.Attr("method", "POST").Attr("enctype", "multipart/form-data")
-                .Display(false).Input.Event(EventType.Change, async (ev) => await SelectedExcelPortLowerliftGoods(ev)).Type("file").Id($"id_{GetHashCode()}").Attr("name", "fileImport").Attr("accept", ".xlsx");
-                _uploaderPortLowerliftGoods = Html.Context as HTMLInputElement;
-            };
-            DOMContentLoaded += () =>
-            {
-                Html.Take("Body").Form.Attr("method", "POST").Attr("enctype", "multipart/form-data")
-                .Display(false).Input.Event(EventType.Change, async (ev) => await SelectedExcelPortLowerliftHollow(ev)).Type("file").Id($"id_{GetHashCode()}").Attr("name", "fileImport").Attr("accept", ".xlsx");
-                _uploaderPortLowerliftHollow = Html.Context as HTMLInputElement;
-            };
         }
 
         public async Task EditLocation(TMS.API.Models.Location entity)
@@ -68,76 +49,83 @@ namespace TMS.UI.Business.Settings
                 });
         }
 
-        private async Task SelectedExcel(Event e)
+        public virtual void CustomMenu()
         {
-            var files = e.Target["files"] as FileList;
-            if (files.Nothing())
+            var gridView = this.FindActiveComponent<GridView>().FirstOrDefault(x => x.GuiInfo.RefName == nameof(Location));
+            if (gridView is null)
             {
                 return;
             }
-
-            var uploadForm = _uploader.ParentElement as HTMLFormElement;
-            var formData = new FormData(uploadForm);
-            var response = await Client.SubmitAsync<List<TMS.API.Models.Location>>(new XHRWrapper
+            gridView.BodyContextMenuShow += () =>
             {
-                FormData = formData,
-                Url = "ImportLocation",
-                Method = HttpMethod.POST,
-                ResponseMimeType = Utils.GetMimeType("xlsx")
+                var menus = new List<ContextMenuItem>();
+                menus.Clear();
+                menus.Add(new ContextMenuItem
+                {
+                    Icon = "fas fa-pen",
+                    Text = "Cập nhật cước",
+                    MenuItems = new List<ContextMenuItem>
+                    {
+                        new ContextMenuItem { Text = "Cước đóng hàng", Click = UpdateQuotationClosing },
+                        new ContextMenuItem { Text = "Cước trả hàng", Click = UpdateQuotationReturn },
+                    }
+                });
+                ContextMenu.Instance.MenuItems = menus;
+            };
+        }
+
+        private void UpdateQuotationReturn(object arg)
+        {
+            Task.Run(async () =>
+            {
+                var gridView = this.FindActiveComponent<GridView>().FirstOrDefault(x => x.GuiInfo.RefName == nameof(Location));
+                if (gridView is null)
+                {
+                    return;
+                }
+                var location = gridView.EntityFocusId;
+                await this.OpenPopup(
+                    featureName: "Quotation Editor",
+                    factory: () =>
+                    {
+                        var type = Type.GetType("TMS.UI.Business.Settings.QuotationEditorBL");
+                        var instance = Activator.CreateInstance(type) as PopupEditor;
+                        instance.Title = "Chỉnh sửa bảng giá trả hàng";
+                        instance.Entity = new Quotation()
+                        {
+                            LocationId = location,
+                            TypeId = 7593
+                        };
+                        return instance;
+                    });
             });
         }
 
-        public void ImportLocation()
+        private void UpdateQuotationClosing(object arg)
         {
-            _uploader.Click();
-        }
-
-        private async Task SelectedExcelPortLowerliftGoods(Event e)
-        {
-            var files = e.Target["files"] as FileList;
-            if (files.Nothing())
+            Task.Run(async () =>
             {
-                return;
-            }
-
-            var uploadForm = _uploaderPortLowerliftGoods.ParentElement as HTMLFormElement;
-            var formData = new FormData(uploadForm);
-            var response = await Client.SubmitAsync<List<TMS.API.Models.Location>>(new XHRWrapper
-            {
-                FormData = formData,
-                Url = "ImportPortLowerliftGoods",
-                Method = HttpMethod.POST,
-                ResponseMimeType = Utils.GetMimeType("xlsx")
+                var gridView = this.FindActiveComponent<GridView>().FirstOrDefault(x => x.GuiInfo.RefName == nameof(Location));
+                if (gridView is null)
+                {
+                    return;
+                }
+                var location = gridView.EntityFocusId;
+                await this.OpenPopup(
+                    featureName: "Quotation Editor",
+                    factory: () =>
+                    {
+                        var type = Type.GetType("TMS.UI.Business.Settings.QuotationEditorBL");
+                        var instance = Activator.CreateInstance(type) as PopupEditor;
+                        instance.Title = "Chỉnh sửa bảng giá đóng hàng";
+                        instance.Entity = new Quotation()
+                        {
+                            LocationId = location,
+                            TypeId = 7592
+                        };
+                        return instance;
+                    });
             });
-        }
-
-        public void ImportPortLowerliftGoods()
-        {
-            _uploaderPortLowerliftGoods.Click();
-        }
-
-        private async Task SelectedExcelPortLowerliftHollow(Event e)
-        {
-            var files = e.Target["files"] as FileList;
-            if (files.Nothing())
-            {
-                return;
-            }
-
-            var uploadForm = _uploaderPortLowerliftHollow.ParentElement as HTMLFormElement;
-            var formData = new FormData(uploadForm);
-            var response = await Client.SubmitAsync<List<TMS.API.Models.Location>>(new XHRWrapper
-            {
-                FormData = formData,
-                Url = "ImportPortLowerliftHollow",
-                Method = HttpMethod.POST,
-                ResponseMimeType = Utils.GetMimeType("xlsx")
-            });
-        }
-
-        public void ImportPortLowerliftHollow()
-        {
-            _uploaderPortLowerliftHollow.Click();
         }
     }
 }
