2a86a192b4739572d79b8f22a089397fa1688933
VinhCQ <quangmvinh1234567890@gmail.com>
2023-04-13 15:12:47 +0700

    fix bug check lockship

--- TMS.API/Controller/TransportationController.cs
+++ TMS.API/Controller/TransportationController.cs
@@ -80,8 +80,7 @@ namespace TMS.API.Controllers
                 || x.Field == nameof(entity.ContainerTypeId)
                 || x.Field == nameof(entity.SocId)) && entity.BookingId != null)
                 {
-                    var bookingList = await db.BookingList.Where(x => x.Id == entity.BookingListId && x.Active).FirstOrDefaultAsync();
-                    if (bookingList != null && bookingList.Submit)
+                    if (entity.LockShip)
                     {
                         throw new ApiException("DSVC này đã được khóa. Vì đã được khóa ở danh sách book tàu.") { StatusCode = HttpStatusCode.BadRequest };
                     }
