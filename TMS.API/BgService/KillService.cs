2f2ae2c0e0fe8d8c166b228f40756466cbd38c03
nguyentrantindev@gmail.com <nguyentrantindev@gmail.com>
2023-04-22 06:48:29 +0700

    kill

--- TMS.API/BgService/KillService.cs
+++ TMS.API/BgService/KillService.cs
@@ -23,7 +23,7 @@ namespace TMS.API.BgService
             while (!stoppingToken.IsCancellationRequested)
             {
                 KillBlockedProcesses();
-                await Task.Delay(2000, stoppingToken);
+                await Task.Delay(1000 * 60, stoppingToken);
             }
         }
 
