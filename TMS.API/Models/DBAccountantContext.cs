df60885b9d75e331f8ead27d751b5c73b18d23bb
VinhCQ <quangmvinh1234567890@gmail.com>
2023-05-24 14:38:49 +0700

    change db

---
+++ TMS.API/Models/DBAccountantContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TMS.API.Models
+{
+    public partial class DBAccountantContext : DbContext
+    {
+        public DBAccountantContext()
+        {
+        }
+
+        public DBAccountantContext(DbContextOptions<DBAccountantContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<SaleAC> Sale { get; set; }
+        public virtual DbSet<Partner> Vendor { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
