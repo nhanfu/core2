1c5e85f7d48f530be27baa1e405667d35348d2c0
Tinnt <nguyentrantindev@gmail.com>
2023-05-24 14:22:41 +0700

    change delete HardDeleteAsync  Allotment

--- TMS.API/Controller/AllotmentController.cs
+++ TMS.API/Controller/AllotmentController.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Enums;
 using Core.Exceptions;
 using Core.Extensions;
+using Core.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TMS.API.Models;
@@ -9,7 +10,7 @@ namespace TMS.API.Controllers
 {
     public class AllotmentController : TMSController<Allotment>
     {
-        public AllotmentController(TMSContext context,EntityService entityService, IHttpContextAccessor httpContextAccessor) : base(context, entityService, httpContextAccessor)
+        public AllotmentController(TMSContext context, EntityService entityService, IHttpContextAccessor httpContextAccessor) : base(context, entityService, httpContextAccessor)
         {
 
         }
@@ -27,8 +28,25 @@ namespace TMS.API.Controllers
             }
             try
             {
+                var exps = await db.Expense.Where(x => x.AllotmentId != null).AsNoTracking().Where(x => ids.Contains(x.AllotmentId.Value)).ToListAsync();
                 var deleteCommand = $"delete from [{typeof(Expense).Name}] where {nameof(Expense.AllotmentId)} in ({string.Join(",", ids)}) ; delete from [{typeof(Allotment).Name}] where Id in ({string.Join(",", ids)})";
                 await ctx.Database.ExecuteSqlRawAsync(deleteCommand);
+                var tranids = exps.Select(x => x.TransportationId).Where(x => x != null).Distinct().ToList();
+                var trans = await db.Transportation.Include(x => x.Expense).Where(x => tranids.Contains(x.Id)).ToListAsync();
+                var expenseTypeIds = exps.Select(x => x.ExpenseTypeId).Where(x => x != null).Distinct().ToList();
+                var expenseTypes = await db.MasterData.AsNoTracking().Where(x => expenseTypeIds.Contains(x.Id)).ToListAsync();
+                foreach (var item in trans)
+                {
+                    var details = new List<PatchUpdateDetail>();
+                    var expenses = item.Expense;
+                    foreach (var itemDetail in expenseTypes.Select(x => x.Additional).Distinct().Where(x => !x.IsNullOrWhiteSpace()).ToList())
+                    {
+                        var expenseTypeThisIds = expenseTypes.Where(x => x.Additional == itemDetail).Select(x => x.Id).Distinct().ToList();
+                        var totalThisValue = expenses.Where(x => expenseTypeThisIds.Contains(x.ExpenseTypeId.Value)).Sum(x => x.TotalPriceAfterTax);
+                        item.SetPropValue(itemDetail, totalThisValue);
+                    }
+                }
+                await db.SaveChangesAsync();
                 return true;
             }
             catch
