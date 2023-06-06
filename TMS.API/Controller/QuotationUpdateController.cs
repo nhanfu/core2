8d8f9a58701129da34ea8a8317ad65a193fdcba1
Tinnt <nguyentrantindev@gmail.com>
2023-06-06 10:02:38 +0700

    fix QuotationUpdate

--- TMS.API/Controller/QuotationUpdateController.cs
+++ TMS.API/Controller/QuotationUpdateController.cs
@@ -82,8 +82,36 @@ namespace TMS.API.Controllers
             }
             var unitPrice1 = entity.UnitPrice1 ?? entity.UnitPrice;
             var unitPrice3 = entity.UnitPrice3 ?? entity.UnitPrice;
-            cmd += $@")
-                insert into Quotation(BranchId
+            cmd += $@")";
+            if (entity.Packing1Ids != null && entity.Packing1Ids.Any() || entity.PackingIds != null && entity.PackingIds.Any())
+            {
+                cmd += $@";WITH cte1 AS
+                            (
+                               SELECT *,
+                                     ROW_NUMBER() OVER (PARTITION BY PackingId,BossId,ContainerTypeId,LocationId,RouteId,RegionId ORDER BY StartDate DESC) AS rn1
+                               FROM Quotation
+                               where TypeId = {entity.TypeId}
+                               and ExportListId = {VendorId}
+                               and StartDate < '{entity.StartDate:yyyy-MM-dd}'";
+                if (entity.ContainerId != null)
+                {
+                    cmd += $@" and ContainerId = {entity.ContainerId}";
+                }
+                if (entity.PackingIds != null && entity.PackingIds.Any())
+                {
+                    cmd += $@" and PackingId not in ({entity.PackingIds.Combine()})";
+                }
+                if (regionIds != null && regionIds.Any())
+                {
+                    cmd += $@" and RegionId in ({regionIds.Combine()})";
+                }
+                if (entity.Packing1Ids != null && entity.Packing1Ids.Any())
+                {
+                    cmd += $@" and PackingId in ({entity.Packing1Ids.Combine()})";
+                }
+                cmd += $@")";
+            }
+            cmd += $@"insert into Quotation(BranchId
             			   ,[TypeId]
             			   ,[RouteId]
             			   ,[ContainerTypeId]
@@ -133,6 +161,61 @@ namespace TMS.API.Controllers
             ,{VendorId}
                             FROM cte
                             WHERE rn = 1";
+
+
+            if (entity.Packing1Ids != null && entity.Packing1Ids.Any() || entity.PackingIds != null && entity.PackingIds.Any())
+            {
+                cmd += $@"insert into Quotation(BranchId
+            			   ,[TypeId]
+            			   ,[RouteId]
+            			   ,[ContainerTypeId]
+            			   ,[PackingId]
+            			   ,[BossId]
+            			   ,[LocationId]
+            			   ,[PolicyTypeId]
+            			   ,[UnitPrice]
+            			   ,[UnitPrice1]
+            			   ,[UnitPrice2]
+            			   ,[StartDate]
+            			   ,[Note]
+            			   ,[Active]
+            			   ,[InsertedDate]
+            			   ,[InsertedBy]
+            			   ,[QuotationUpdateId]
+            			   ,[UnitPrice3]
+            			   ,[ParentId]
+            			   ,[IsParent]
+            			   ,[RegionId]
+            			   ,[DistrictId]
+            			   ,[ProvinceId]
+                           ,[ExportListId])
+                SELECT [BranchId]
+            ,[TypeId]
+            ,[RouteId]
+            ,[ContainerTypeId]
+            ,[PackingId]
+            ,[BossId]
+            ,[LocationId]
+            ,[PolicyTypeId]
+            ,case when [UnitPrice] > 0 then case when {(entity.IsAdd ? 1 : 0)} = 1 then [UnitPrice] else [UnitPrice] end else [UnitPrice] end
+            ,isnull(case when [UnitPrice1] > 0 then case when {entity.TypeId} = 7592 or {entity.TypeId} = 7593 or {entity.TypeId} = 7594 or {entity.TypeId} = 7596 then case when {(entity.IsAdd ? 1 : 0)} = 1 then [UnitPrice1] else [UnitPrice1] end else [UnitPrice1] end end,0)
+            ,UnitPrice2
+            ,'{entity.StartDate:yyyy-MM-dd}' as [StartDate]
+            ,[Note]
+            ,[Active]
+            ,GETDATE() as InsertedDate
+            ,{UserId}
+            ,{entity.Id}
+            ,isnull(case when [UnitPrice3] > 0 then case when {entity.TypeId} = 7592 then case when {(entity.IsAdd ? 1 : 0)} = 1 then [UnitPrice3] else [UnitPrice3] end else [UnitPrice3] end end,0)
+            ,[ParentId]
+            ,[IsParent]
+            ,[RegionId]
+            ,[DistrictId]
+            ,[ProvinceId]
+            ,{VendorId}
+                            FROM cte1
+                            WHERE rn1 = 1";
+            }
             await db.Database.ExecuteSqlRawAsync(cmd);
             return true;
         }
