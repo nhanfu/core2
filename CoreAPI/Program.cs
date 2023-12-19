2ad41b3f792c8f253bdd077f0cb54251eade760d
nhanjs <nhann@softek.vn>
2023-12-19 16:08:26 +0700

    Using new type of setup webapp

--- CoreAPI/Program.cs
+++ CoreAPI/Program.cs
@@ -1,49 +1,116 @@
-using Microsoft.AspNetCore;
+using Core.Extensions;
+using Core.Services;
+using Core.Websocket;
+using CoreAPI.Middlewares;
+using Hangfire;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IO.Compression;
+using System.Text;
 
-namespace Core
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var _conf = builder.Configuration;
+services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var webConfig = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .Build();
-            var host = WebHost.CreateDefaultBuilder(args)
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    logging.AddConsole();
-                })
-                .UseConfiguration(webConfig);
-            host.UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>();
-            host.Build().Run();
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+services.Configure<IISServerOptions>(options =>
+{
+    options.AutomaticAuthentication = false;
+});
+services.AddDistributedMemoryCache();
+services.AddLogging(config =>
+{
+    config.ClearProviders();
+    config.AddConfiguration(_conf.GetSection("Logging"));
+    config.AddDebug();
+    config.AddEventSourceLogger();
+});
+services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Fastest);
+services.AddResponseCompression(options =>
+{
+    options.Providers.Add<GzipCompressionProvider>();
+});
+services.AddWebSocketManager();
+services.AddMvc(options =>
+{
+    options.EnableEndpointRouting = false;
+})
+.AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ContractResolver = new IgnoreNullOrEmptyEnumResolver();
+    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
 
-            // var builder = WebApplication.CreateBuilder(args);
+    options.SerializerSettings.Converters.Add(new DateParser());
+    options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+    options.SerializerSettings.DateParseHandling = DateParseHandling.DateTimeOffset;
+    options.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+});
+services.AddHangfire(configuration => configuration.UseSqlServerStorage(_conf.GetConnectionString($"Log")));
+var tokenOptions = new TokenValidationParameters()
+{
+    ValidIssuer = _conf["Tokens:Issuer"],
+    ValidAudience = _conf["Tokens:Issuer"],
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_conf["Tokens:Key"])),
+    ClockSkew = TimeSpan.Zero
+};
+services.AddSingleton(tokenOptions);
+services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = tokenOptions;
 
-            // // Add services to the container.
-            // builder.Services.AddRazorPages();
+});
+services.AddDistributedMemoryCache();
+services.AddHttpContextAccessor();
 
-            // var app = builder.Build();
+// the instance created for each request
+services.AddScoped<WebSocketService>();
+services.AddScoped<TaskService>();
+services.AddScoped<UserService>();
 
-            // // Configure the HTTP request pipeline.
-            // if (!app.Environment.IsDevelopment())
-            // {
-            //     app.UseExceptionHandler("/Error");
-            //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //     app.UseHsts();
-            // }
+var app = builder.Build();
 
-            // app.UseHttpsRedirection();
-            // app.UseStaticFiles();
+app.UseCors("MyPolicy");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseResponseCompression();
+app.UseStaticFiles();
+app.UseHangfireDashboard();
+app.UseHangfireServer();
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseMiddleware<LoadBalaceMiddleware>();
+UseSocket(app);
+app.UseAuthentication();
+app.UseMvc();
+app.UseRouting();
 
-            // app.UseRouting();
+app.Run();
 
-            // app.UseAuthorization();
-
-            // app.MapRazorPages();
-
-            // app.Run();
-        }
-    }
+void UseSocket(IApplicationBuilder app)
+{
+    app.UseWebSockets();
+    var serviceScopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
+    var serviceProvider = serviceScopeFactory.CreateScope().ServiceProvider;
+    app.Map("/task", app => app.UseMiddleware<WebSocketManagerMiddleware>(serviceProvider.GetService<WebSocketService>()));
 }
