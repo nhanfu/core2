7176bed0bab4dc3d2a96d7157f9cd80f0d8916fa
ThanhLong <lelong236610@gmail.com>
2024-04-23 11:52:42 +0700

    update Migrate Textbox.cs to textbox.js

--- CoreAPI/wwwRoot/js/core/textbox.js
+++ CoreAPI/wwwRoot/js/core/textbox.js
@@ -1,18 +1,23 @@
 import EditableComponent from './editableComponent.js';
 import { Html } from "./utils/html.js";
 import { Utils } from "./utils/utils.js";
+import { ValidationRule } from "./models/validationRule.js";
+import { LangSelect } from "./langSelect.js";
+import { Client } from "./clients/client.js";
+
 
 class Textbox extends EditableComponent {
     constructor(meta, ele) {
         super(meta, ele);
-        this.DefaultValue = '';
         this.Input = null;
         this.TextArea = null;
         this.MultipleLine = false;
         this.Password = false;
         this._value = null;
-        this._text = '';
         this._oldText = '';
+        this._text = '';
+        this.Text = '';
+        this.DefaultValue = '';
         if (ele instanceof HTMLInputElement) {
         this.Input = ele;
         } 
@@ -22,7 +27,7 @@ class Textbox extends EditableComponent {
     }
     Render() {
         this.SetDefaultVal();
-        var val = GetPropValue(this.Entity, this.FieldName);
+        var val = this.Entity && Utils.GetPropValue(this.Entity, this.FieldName);
         if (!val && typeof val === "string" && this.EditForm != null && this.EditForm.Feature != null && !this.EditForm.Feature.IgnoreEncode) {
             const ecode = Utils.DecodeSpecialChar(val);
             const Encode = Utils.EncodeSpecialChar(ecode);
@@ -59,12 +64,12 @@ class Textbox extends EditableComponent {
             this.TextArea.onchange += (e) => PopulateUIChange(EventType.Change);
         }
         if (this.Meta.ChildStyle && this.Meta.ChildStyle.trim() !== "") {
-            var fn;
-            if (Utils.IsFunction(Meta.ChildStyle, function(result) {
-                fn = result;
-            })) {
-                fn.call(this, Entity, this.Element).toString();
-            }
+            Utils.IsFunction(Meta.ChildStyle, function(fn) {
+                if (fn !== null && typeof fn === 'function') {
+                    fn.call(this, Entity, this.Element).toString();
+                }
+            });
+            
         }
         if (this.Password)
         {
@@ -87,12 +92,109 @@ class Textbox extends EditableComponent {
         this._text = this.Password ? this._text : (shouldTrim ? this._text?.trim() : this._text);
         if (this.Meta.UpperCase && this._text != null)
             {
-                // Text = _text.ToLocaleUpperCase();
+                this.Text = this._text.toLocaleUpperCase();
             }
             this._value = (this.EditForm != null && this.EditForm.Feature != null && this.EditForm.Feature.IgnoreEncode) ? this._text : EncodeSpecialChar(this._text);
             this.Entity && SetComplexPropValue(this.Entity,this.FieldName, this._value);
             this.Dirty = true;
-            // this.UserInput?.Invoke(new ObservableArgs { NewData = _text, OldData = _oldText, EvType = type });
+            this.UserInput?.Invoke({ NewData: this._text, OldData: this._oldText, EvType: type });
+            this.PopulateFields();
+            this.DispatchEvent(this.Meta.Events, type, this.Entity);
+
+    }
+    UpdateView(force = false, dirty = null, ...componentNames) {
+        Value = this.Entity && Utils.GetPropValue(this.Entity,this.FieldName);
+        if (!Dirty)
+        {
+            this.DOMContentLoaded?.Invoke();
+            this.OldValue = this._text;
+        }
+    }
+    validateRegEx(value, regText) {
+        if (value === null) {
+            return true;
+        }
+        var regEx = new RegExp(regText);
+        var res = regEx.test(value);
+        var rule = this.ValidationRules[ValidationRule.RegEx];
+        if (!res && rule.RejectInvalid) {
+            var end = this.Input.SelectionEnd;
+            this.Text = this._oldText;
+            this._value = this._oldText;
+            this.Input.SelectionStart = end;
+            this.Input.SelectionEnd = end;
+            return regEx.test(this._oldText);
+        }
+        return res;
+    }
+    ValidateAsync() {
+        if (this.ValidationRules.Nothing()) {
+            return true;
+        }
+        const tcs = new Promise((resolve, reject) => {
+            ValidationResult.Clear();
+            this.Validate(ValidationRule.MinLength, this._text, (value, minLength) => this._text != null && this._text.length >= minLength);
+            this.Validate(ValidationRule.CheckLength, this._text, (text, checkLength) => this._text == null || this._text == "" || this._text.length == checkLength);
+            this.Validate(ValidationRule.MaxLength, this._text, (text, maxLength) => this._text == null || this._text.length <= maxLength);
+            this.Validate(ValidationRule.RegEx, this._text, ValidateRegEx);
+            this.ValidateRegEx(this._text); 
+            this.ValidateRequired(this.Text);
+            this.ValidateUnique().then(() => {
+                resolve(this.IsValid);
+            });
+        });
+    
+        return tcs;
+    }
+    ValidateUnique() {
+        if (this.ValidationRules.hasOwnProperty(ValidationRule.Unique)) {
+            return Promise.resolve(true);
+        }
+        var rule = this.ValidationRules[ValidationRule.Unique];
+        if (rule === null || _text.trim() !== "")
+        {
+                return Promise.resolve(true);
+        }
+        var isFn;
+        var fn;
+        Utils.IsFunction(Meta.PreQuery, function(result) {
+            isFn = result !== null && typeof result === 'function';
+            if (isFn) {
+                fn = result;
+            }
+        });
+        var table = !this.Meta.RefName ? this.Meta.RefName : this.EditForm.Feature.EntityName;
+        const submit = {
+            ComId : this.Meta.Id,
+            Params : isFn ? JSON.Stringify(fn.Call(null, this)) : null,
+            MetaConn : this.MetaConn,
+            DataConn : this.DataConn,
+        };
+        var tcs = new Promise((resolve, reject) => {
+            Client.Instance.ComQuery(submit)
+            .then(ds => {
+                var exists = ds.length > 0 && ds[0].length > 0;
+                if (exists) {
+                    this.ValidationResult.TryAdd(ValidationRule.Unique, `${rule.Message} ${LangSelect.Get(Meta.Label)} ${this._text}`);
+                } else {
+                    this.ValidationResult.Remove(ValidationRule.Unique);
+                }
+                resolve(true);
+            });
+        });
+        
+        return tcs;
+    }
+    SetDisableUI(value) {
+        if (this.Input != null)
+        {
+            this.Input.ReadOnly = value;
+        }
+
+        if (this.TextArea != null)
+        {
+            this.TextArea.ReadOnly = value;
+        }
     }
 }
 
