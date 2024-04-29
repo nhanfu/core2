cc3d25cbc8030d068e51cb661624a1c6d7655c47
ThanhLong <lelong236610@gmail.com>
2024-04-29 16:36:17 +0700

    add spinner.js

---
+++ CoreAPI/wwwRoot/js/core/spinner.js
@@ -0,0 +1,71 @@
+
+
+class Spinner {
+    /** @type {Spinner} */
+    static _instance = null;
+    /** @type {HTMLElement} */
+    static _span = null;
+    /** @type {HTMLElement} */
+    static _backdrop = null;
+    static _hiddenAwaiter = null;
+    constructor() {
+       
+    }
+    static init() {
+        if (this._instance !== null) {
+            return;
+        }
+
+        this._instance = new Spinner();
+        let existing = document.getElementById("spinner");
+        if (!existing) {
+            let div = document.createElement("div");
+            div.className = "backdrop";
+            div.style.background = "transparent !important";
+            document.body.appendChild(div);
+
+            let span = document.createElement("span");
+            span.className = "spinner";
+            div.appendChild(span);
+
+            this._span = span;
+            this._backdrop = div;
+        } else {
+            this._span = existing;
+            this._backdrop = existing.previousElementSibling;
+        }
+        this._span.style.display = "none";
+        this._backdrop.style.display = "none";
+    }
+
+    static appendTo(node, lockScreen = true, autoHide = true, timeout = 7000) {
+        if (!this._span) {
+            return;
+        }
+        if (this._span.parentElement === node && this._span.style.display === "") {
+            return;
+        }
+        if (node) {
+            node.appendChild(this._span);
+        } else {
+            document.body.appendChild(this._span);
+        }
+
+        this._span.style.display = "";
+        if (lockScreen) {
+            this._backdrop.style.display = "";
+        }
+        if (!autoHide) {
+            return;
+        }
+        window.clearTimeout(this._hiddenAwaiter);
+        this._hiddenAwaiter = window.setTimeout(() => this.hide(), timeout);
+    }
+
+    static hide() {
+        if (this._span) {
+            this._span.style.display = "none";
+            this._backdrop.style.display = "none";
+        }
+    }
+}
