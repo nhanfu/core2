c1a6e3995d218ca11888949727b9f54b42008117
nguyentrantindev <nguyentrantindev@gmail.com>
2024-10-09 17:02:40 +0700

    fix bug

--- CoreAPI/Services/PdfService.cs
+++ CoreAPI/Services/PdfService.cs
@@ -14,12 +14,17 @@ namespace CoreAPI.Services
         {
             var component = await BgExt.ReadDsAs<Component>($"SELECT * FROM [Component] where Id = '{createHtmlVM.ComId}'", conn);
             var components = await BgExt.ReadDsAsArr<Component>($"SELECT * FROM [Component] where Label is not null and Label != '' and FeatureId = '{component.FeatureId}' and ComponentGroupId is not null and ComponentType not in ('Button','Section','GridView')", conn);
+            var gridPolicys = await BgExt.ReadDsAsArr<Component>($"SELECT * FROM [Component] where Label is not null and Label != '' and FeatureId = '{component.FeatureId}' and EntityId is not null and ComponentType not in ('Button','Section','GridView')", conn);
             var dirCom = components.DistinctBy(x => x.Label).ToDictionary(x => x.Label);
             HtmlDocument document = new HtmlDocument();
             document.LoadHtml(component.Template);
             foreach (var item in document.DocumentNode.ChildNodes)
             {
                 ReplaceNode(createHtmlVM, item, dirCom);
+            }
+            foreach (var item in document.DocumentNode.ChildNodes)
+            {
+                ReplaceTableNode(createHtmlVM, item, dirCom);
             }
             return document.DocumentNode.InnerHtml;
         }
@@ -104,6 +109,101 @@ namespace CoreAPI.Services
             foreach (var item in htmlNode.ChildNodes)
             {
                 ReplaceNode(createHtmlVM, item, dirCom);
+            }
+        }
+
+        private HtmlNode FindClosest(HtmlNode node, string element)
+        {
+            while (node != null && node.Name != element)
+            {
+                node = node.ParentNode;
+            }
+            return node;
+        }
+
+        private void ReplaceTableNode(CreateHtmlVM createHtmlVM, HtmlNode htmlNode, Dictionary<string, Component> dirCom)
+        {
+            var dollarCurlyBraceRegex = new Regex(@"\{(.+?)\}");
+            var matches = dollarCurlyBraceRegex.Matches(htmlNode.InnerHtml);
+            HtmlNode lastTr = null;
+            foreach (Match match in matches)
+            {
+                if (match.Success)
+                {
+                    var currentTr = FindClosest(htmlNode, "tr");
+                    if (lastTr != currentTr)
+                    {
+                        lastTr = currentTr;
+                        var valueWithinCurlyBraces = match.Groups[1].Value;
+                        var htmlDoc = new HtmlDocument();
+                        htmlDoc.LoadHtml(valueWithinCurlyBraces);
+                        string plainText = htmlDoc.DocumentNode.InnerText;
+                        if (plainText.Contains("."))
+                        {
+                            var fields = plainText.Split(".");
+                            var mainObject = JsonConvert.DeserializeObject<Dictionary<string, object>[]>(JsonConvert.SerializeObject(createHtmlVM.Data[fields[0]]));
+                            var headers = JsonConvert.DeserializeObject<List<Component>>(JsonConvert.SerializeObject(createHtmlVM.Data[fields[0] + "Header"]));
+                            foreach (var field in mainObject)
+                            {
+                                var newRow = currentTr.CloneNode(true);
+                                foreach (var cell in newRow.ChildNodes)
+                                {
+                                    var valueWithinCurlyBraces2 = match.Groups[1].Value;
+                                    htmlDoc = new HtmlDocument();
+                                    htmlDoc.LoadHtml(valueWithinCurlyBraces2);
+                                    var matches2 = dollarCurlyBraceRegex.Match(htmlNode.InnerHtml);
+                                    string plainText2 = matches2.Groups[1].Value;
+                                    var field2s = plainText2.Split(".");
+                                    var curentComponent = headers.FirstOrDefault(x => x.Label == field2s[1]);
+                                    if (curentComponent != null)
+                                    {
+                                        try
+                                        {
+                                            var currentData = field[curentComponent.FieldName];
+                                            switch (curentComponent.ComponentType)
+                                            {
+                                                case "Datepicker":
+                                                    currentData = currentData is null ? "" : Convert.ToDateTime(currentData).ToString("dd/MM/yyyy");
+                                                    break;
+                                                case "Number":
+                                                    currentData = currentData is null ? "" : Convert.ToDecimal(currentData).ToString(curentComponent.FormatData ?? "N0");
+                                                    break;
+                                                case "Dropdown":
+                                                    var displayField = curentComponent.FieldName;
+                                                    var containId = curentComponent.FieldName.EndsWith("Id");
+                                                    if (containId)
+                                                    {
+                                                        displayField = displayField.Substring(0, displayField.Length - 2);
+                                                    }
+                                                    else
+                                                    {
+                                                        displayField = displayField + "MasterData";
+                                                    }
+                                                    currentData = FormatString(curentComponent.FormatData, JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(field.GetValueOrDefault(displayField))));
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
+                                            htmlNode.InnerHtml = htmlNode.InnerHtml.Replace($"{{{valueWithinCurlyBraces}}}", currentData?.ToString() ?? string.Empty);
+                                        }
+                                        catch (Exception e)
+                                        {
+                                            throw;
+                                        }
+                                        
+                                    }
+                                }
+                                var currentTbody = FindClosest(htmlNode, "tbody");
+                                newRow.InnerHtml = FormatString(newRow.InnerHtml, field);
+                                currentTbody.InsertBefore(newRow, lastTr);
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (var item in htmlNode.ChildNodes)
+            {
+                ReplaceTableNode(createHtmlVM, item, dirCom);
             }
         }
     }
