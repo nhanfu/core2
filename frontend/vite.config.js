3ec16a0752646443174b94b25416ca35e20f1627
nhanjs <nhannguyen@dayoneteams.com>
2025-04-17 17:45:59 +0700

    Allow vite to access externally

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
-  ]
+  ],
+  server: {
+    host: true, // Allows the server to be accessed externally
+    strictPort: false // Disables strict host checking
+  }
 });
