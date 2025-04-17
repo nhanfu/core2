4f615c1c6ab94eb203e1adb8670434a040b1ba85
nhanjs <nhannguyen@dayoneteams.com>
2025-04-17 08:17:52 +0700

    Remove some config file that not used

--- test.bat
+++
@@ -1,4 +0,0 @@
-dotnet build CoreAPI/CoreAPI.csproj
-cmd /c "start ""API"" powershell.exe -NoExit -Command ""dotnet run --project CoreAPI/CoreAPI.csproj --no-build --urls=http://localhost:5100 --Role=API"""
-cmd /c "start ""API"" powershell.exe -NoExit -Command ""dotnet run --project CoreAPI/CoreAPI.csproj --no-build --urls=http://localhost:5101 --Role=API"""
-cmd /c "start ""Balancer"" powershell.exe -NoExit -Command ""dotnet run --project CoreAPI/CoreAPI.csproj --no-build --urls=https://localhost:5033 --Role=Balancer"""
