05e19910db98e7753947182bf890147698911d12
Nhan Nguyen <nhan.n@arbin.com>
2026-02-24 16:55:52 +0700

    Remove script runner and samples client projects

--- svc/examples/client-b.ts
+++
@@ -1,5 +0,0 @@
-import { FileMetadataStore } from "../src/index.js";
-
-const store = new FileMetadataStore(process.cwd(), "system");
-const feature = await store.getFeature("demo");
-console.log("Feature name", feature?.Name ?? null);
